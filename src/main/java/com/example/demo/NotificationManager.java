581d79a3f218e6ca16c7e6cabe2c537a0d718c53
Masoud <mmanson519@gmail.com>
2026-01-05 22:52:03 +0330

    two

--- src/main/java/com/example/demo/NotificationManager.java
+++ src/main/java/com/example/demo/NotificationManager.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Lazy;
@@ -12,6 +14,16 @@ public class NotificationManager {
     public NotificationManager(Notificationservice notificationservice){
         this.notificationservice = notificationservice;
         System.out.println("main");
+    }
+
+    @PostConstruct
+    public void pre(){
+        System.out.println("pre pre pre pre pre pre");
+    }
+
+    @PreDestroy
+    public void des(){
+        System.out.println("des des des des des");
     }
 
     public void sendNotification(){
